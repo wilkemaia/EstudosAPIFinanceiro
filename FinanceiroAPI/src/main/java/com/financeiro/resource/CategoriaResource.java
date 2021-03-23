abfce4180d72bddee3916abe47c00aae8d677cca
Wilke rodrigues maia <wilkemaia2@gmail.com>
2021-03-23 14:55:17 -0300

    Segundo Comite- Consumindo Api de Categoria

---
+++ FinanceiroAPI/src/main/java/com/financeiro/resource/CategoriaResource.java
@@ -0,0 +1,25 @@
+package com.financeiro.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.financeiro.model.Categoria;
+import com.financeiro.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+	
+	   @Autowired
+	   private CategoriaRepository categoriaRepository;
+	   
+	   @GetMapping
+	   public List<Categoria>listar(){
+		  return categoriaRepository.findAll(); 
+	   }
+	
+}
