99a7c341cb4e1c7b7fcddcc22471c634c9367932
Wilke rodrigues maia <wilkemaia2@gmail.com>
2021-03-24 08:44:51 -0300

    Atualizando Controller Categoria, para o método Post

--- FinanceiroAPI/src/main/java/com/financeiro/resource/CategoriaResource.java
+++ FinanceiroAPI/src/main/java/com/financeiro/resource/CategoriaResource.java
@@ -1,11 +1,20 @@
 package com.financeiro.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.financeiro.model.Categoria;
 import com.financeiro.repository.CategoriaRepository;
@@ -22,4 +31,15 @@ public class CategoriaResource {
 		  return categoriaRepository.findAll(); 
 	   }
 	
+	   
+	   @PostMapping
+	   public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria,HttpServletResponse response) {
+		   
+		  Categoria categoriaSalva = categoriaRepository.save(categoria);
+		  URI uri =ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				   .buildAndExpand(categoriaSalva.getCodigo()).toUri();
+		   
+		  response.setHeader("Location", uri.toASCIIString());
+		  return ResponseEntity.created(uri).body(categoriaSalva);
+	   }
 }
