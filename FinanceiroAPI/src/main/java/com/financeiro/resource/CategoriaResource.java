953e7d4b59f8f6b0ec04d16a3df9e0b68f09cafd
Wilke rodrigues maia <wilkemaia2@gmail.com>
2021-03-24 09:40:01 -0300

    Atualizando Controller Categoria BuscarPeloCodigo, para o método Get

--- FinanceiroAPI/src/main/java/com/financeiro/resource/CategoriaResource.java
+++ FinanceiroAPI/src/main/java/com/financeiro/resource/CategoriaResource.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,5 +42,14 @@ public class CategoriaResource {
 		   
 		  response.setHeader("Location", uri.toASCIIString());
 		  return ResponseEntity.created(uri).body(categoriaSalva);
+	   }
+	   
+	   @GetMapping("/{codigo}")
+       public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+		   
+		   return this.categoriaRepository.findById(codigo)
+			  .map(categoria -> ResponseEntity.ok(categoria))
+		      .orElse(ResponseEntity.notFound().build());
+		   
 	   }
 }
